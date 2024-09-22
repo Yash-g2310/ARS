378c73d1274cbba412927437174badb8842a9557
Yash-g2310 <yg351124@gmail.com>
2024-09-23 05:18:13 +0530

    created apps

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth import models
 
 # Create your models here.
+
+class UserProfile(models.AbstractUser):
+    user_bio = models.CharField( max_length=150)
+    phone_number = models.In
