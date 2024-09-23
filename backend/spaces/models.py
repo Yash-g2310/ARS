4854e26b15e9f62507af8d65aae93685f5fdecb2
Yash-g2310 <yg351124@gmail.com>
2024-09-24 03:48:47 +0530

    created Spaces app class

--- backend/spaces/models.py
+++ backend/spaces/models.py
@@ -17,3 +17,49 @@ class Spaces(models.Model):
     def __str__(self) :
         return self.space_name
     
+class SubSpaces(models.Model):
+    space = models.ForeignKey(Spaces, on_delete=models.CASCADE)
+    SubSpace_name = models.CharField(max_length=50)
+    SubSpace_bio = models.CharField(max_length= 150)
+    create_date = models.DateField(default= timezone.now)
+    reviewer_count = models.IntegerField(default=0)
+    reviewee_count = models.IntegerField(default=0) 
+    group_count = models.IntegerField(default=0)    
+    # subspace_logo
+    
+    def __str__(self) :
+        return self.SubSpace_name
+
+
+class SpaceMembers:
+    space = models.ForeignKey(Spaces, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    join_date = models.DateField(timezone.now)
+
+class SubSpaceMembers:
+    REVIEWEE = 'reviewee'
+    REVIEWER = 'reviewer'
+    ROLE_CHOICES = [
+        ('reviewer','Reviewer'),
+        ('reviewee','Reviewee'),
+    ]
+    spaceMember = models.ForeignKey(SpaceMembers,on_delete=models.CASCADE)
+    sub_space = models.ForeignObject(SubSpaces,on_delete=models.CASCADE)
+    join_date = models.DateField(default=timezone.now)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES,default=REVIEWEE)
+    
+class Group:
+    space = models.ForeignKey(Spaces,on_delete=models.CASCADE,null=True)
+    subspace = models.ForeignKey(SubSpaces,on_delete=models.CASCADE,null=True)
+    group_name = models.CharField(max_length=50)
+    create_date = models.DateField(default=timezone.now)
+    member_count =models.IntegerField(default=0)
+    # group_logo
+    
+    def __str__(self):
+        return self.group_name
+
+class GroupMember:
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    join_date = models.DateField(default=timezone.now)
