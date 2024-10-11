0caa9d02fc58ea17d03c6d6db441661f4f1cd5d2
Yash-g2310 <yg351124@gmail.com>
2024-10-11 23:15:44 +0530

    configure time, phone number validation, media uploads till spaces

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_bio = models.TextField( blank=True,null=True)
-    phone_number = models.CharField( max_length=15,blank=True,null=True)
+    phone_number = PhoneNumberField()
     profile_image = models.ImageField(upload_to='user_profile/',null=True, blank=True)
     background_image = models.ImageField(upload_to='user_background/',null=True, blank=True)
     department = models.CharField( max_length=150,blank=True,null=True)
